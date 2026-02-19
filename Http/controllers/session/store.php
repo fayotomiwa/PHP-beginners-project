e43df311924272a3ba9bc9fc3a5cf894e58e4d9d
Fayotomiwa <iyiolafolarin@gmail.com>
2026-02-19 04:53:32 +0100

    rearranging of foldes and creating http and forms

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -5,23 +5,19 @@
 use Core\App;
 use Core\Database;
 use Core\Validator;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$errors = [];
-if (!Validator::email($email)) {
-  $errors['email'] = 'Please provide a valid email address';
-}
-if (!Validator::string($password)) {
-  $errors['password'] = 'Please provide a valid password.';
-}
+$form = new LoginForm();
 
-if (! empty($errors)) {
-  return view('sessions/create.view.php', [
-    'errors' => $errors
+if (! $form->validate($email, $password)) 
+{
+  return view('session/create.view.php', [
+    'errors' => $form->errors()
   ]);
 }
 
