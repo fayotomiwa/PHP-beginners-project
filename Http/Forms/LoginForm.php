f2b99c3ed0f01aaeb40b97ecca44475a20393a46
Fayotomiwa <iyiolafolarin@gmail.com>
2026-02-25 14:12:15 +0100

    Separating Logic From the Template

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -1,21 +1,39 @@
 <?php
 
 namespace Http\Forms;
+use Core\ValidationException;
 use Core\Validator;
 
 class LoginForm
 {
+  // public $attributes = [];
   protected $errors = [];
-  public function validate($email, $password)
+  
+  public function __construct(public array $attributes)
   {
-    if (!Validator::email($email)) {
+    $this->attributes = $attributes;
+    if (!Validator::email($attributes['email'])) {
       $this->errors['email'] = 'Please provide a valid email address';
     }
-    if (!Validator::string($password)) {
+    if (!Validator::string($attributes['password'])) {
       $this->errors['password'] = 'Please provide a valid password.';
     }
+  }
+  public static function validate($attributes)
+  {
+    $instance = new static($attributes);  
 
-    return empty($this->errors);
+    return $instance->failed() ? $instance->throw() : $instance;
+  }
+
+  public function throw()
+  {
+    ValidationException::throw($this->errors(), $this->attributes);
+  }
+
+  public function failed()
+  {
+    return count($this->errors);
   }
 
   public function errors()
@@ -26,5 +44,7 @@ class LoginForm
   public function error($field, $message)
   {
     $this->errors[$field] = $message;
+
+    return $this;
   }
 }
--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -4,27 +4,24 @@
 
 use Core\Authenticator;
 use Http\Forms\LoginForm;
-use Core\Session;
 
-$email = $_POST['email'];
-$password = $_POST['password'];
 
-$form = new LoginForm();
+$form = LoginForm::validate($attributes = [
+  'email' => $_POST['email'],
+  'password' => $_POST['password']
+]);
 
-if ($form->validate($email, $password)) 
-{
-  if ((new Authenticator)->attempt($email, $password)) {
-      redirect('/');
-  } 
-      $form->error('email', 'No matching account found for that email address and password.');
-  }
+$signedIn = (new Authenticator)->attempt(
+  $attributes['email'], $attributes['password']
+);
 
-  Session::flash('errors', $form->errors());
-  Session::flash('old', [
-    'email' => $_POST['email']
-  ]);
+if (! $signedIn) {
+  $form->error(
+    'email', 'No matching account found for that email address and password.'
+  )->throw();
+} redirect('/');
 
-  return redirect('/login');
+
 
 // return view('session/create.view.php', [
 //   'errors' => $form->errors()
